0796fce499f3a379ac37e2542fdcd7bcf48a95e0
basharelmetwali <96265741+basharelmetwali@users.noreply.github.com>
2021-12-16 23:15:27 +0200

    Add files via upload

---
+++ CIS FORM/js task/p1.js
@@ -0,0 +1,5 @@
+function triangleArea(a,b)
+{
+return 0.5 *a*b;
+}
+console.log("The area of triangle  " +triangleArea(5,4))
---
+++ CIS FORM/js task/p2.js
@@ -0,0 +1,12 @@
+function str(a,b){
+    if(a.length == b.length){
+    
+        return true;
+    
+    }
+    else
+    return false;
+    }
+    console.log(str("hello","hell"))
+    
+    
